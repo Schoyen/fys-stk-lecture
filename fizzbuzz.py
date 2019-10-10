7d82b369dcf9eacbfb60649d8ba4a19c35d93f16
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-10-10 13:32:39 +0200

    Add fizz buzz script

---
+++ fizzbuzz.py
@@ -0,0 +1,42 @@
+import numpy as np
+import tensorflow as tf
+
+import sklearn.neural_network
+import sklearn.model_selection
+
+
+NUM_DIGITS = 10
+NUM_VALUES = int(2 ** NUM_DIGITS)
+
+
+def binary_encoding(x, num_digits):
+    # Make digits one-hot encoded
+    pass
+
+
+def fizz_buzz_one_hot(x):
+    # One-hot encode fizz buzz solution
+    pass
+
+
+def fizz_buzz(x: int, ind: np.array) -> str:
+    # Output fizz buzz response from one-hot encoded solution
+    pass
+
+
+# Create training and test data
+
+# Train (sklearn) neural network classifier
+
+# Output accuracy
+
+# Output response of the first 100 numbers
+
+
+# Train (Tensorflow/Keras) neural network classifier
+
+# Compile model
+
+# Fit to training data
+
+# Output response of the first 100 numbers
