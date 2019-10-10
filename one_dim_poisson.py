2cd33ab59c2a0658121c8e9e526e8a26f963dfcf
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-10-10 14:15:22 +0200

    Add script for the one-dimensional Poisson solver

---
+++ one_dim_poisson.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+tf.keras.backend.set_floatx("float64")
+
+
+# Define analytic solution
+
+# Define grid
+
+# Define model
+
+# Define right-hand side solution
+
+# Define trial solution
+
+# Define loss function
+
+# Define gradient method
+
+# Initialize model and optimizer
+
+# Run training loop
+# Apply gradients in optimizer
+# Output loss improvement
+
+
+# Plot solution on larger grid
